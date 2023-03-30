e4c84903a45eb2c09909cfa1510b4f5da64a8e39
banzarkhan <banzarhanova93@gmail.com>
2023-03-30 10:54:49 +0200

    Initial commit

---
+++ UIKitApp/UIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Ariuna Banzarkhanova on 30/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
